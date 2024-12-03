801931cef2174bba0b3af7626aa6848985dd7dfb
gdevgproject <gdev.gproject@gmail.com>
2024-12-03 15:28:53 +0700

    Adding a Loading State

--- 10-use-popcorn/src/App.js
+++ 10-use-popcorn/src/App.js
@@ -55,15 +55,18 @@ const KEY = "3efe45ba";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = "interstellar";
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const response = await fetch(
         `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await response.json();
       setMovies(data.Search);
+      setIsLoading(false);
     }
 
     fetchMovies();
@@ -76,9 +79,7 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMoviesList watched={watched} />
@@ -86,6 +87,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 function NavBar({ children }) {
