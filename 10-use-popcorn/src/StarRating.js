50be6c7e4140dfc4f756c3cdf8a4bafc75ad4154
gdevgproject <gdev.gproject@gmail.com>
2024-12-05 13:59:57 +0700

    Complete popcorn project

--- 10-use-popcorn/src/StarRating.js
+++ 10-use-popcorn/src/StarRating.js
@@ -30,28 +30,29 @@ export default function StarRating({
   defaultRating = 0,
   onSetRating,
 }) {
-  // maxRating: Tổng số sao
   const [rating, setRating] = useState(defaultRating);
-  // rating: Vị trí sao được chọn
   const [tempRating, setTempRating] = useState(0);
+
   function handleRating(rating) {
     setRating(rating);
     onSetRating(rating);
   }
+
   const textStyle = {
     lineHeight: "1",
-    margin: 0,
+    margin: "0",
     color,
     fontSize: `${size / 1.5}px`,
   };
+
   return (
     <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onRate={() => handleRating(i + 1)}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onRate={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
             color={color}
@@ -69,14 +70,13 @@ export default function StarRating({
 }
 
 function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
-  // onRate: hàm cập nhật rating (vị trí sao được chọn)
-  // full: Đúng Sai (trả về sao đầy, sai thì sao rỗng)
   const starStyle = {
     width: `${size}px`,
     height: `${size}px`,
     display: "block",
     cursor: "pointer",
   };
+
   return (
     <span
       role="button"
@@ -112,3 +112,24 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
     </span>
   );
 }
+
+/*
+FULL STAR
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 20 20"
+  fill="#000"
+  stroke="#000"
+>
+  <path
+    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+  />
+</svg>
+
+
+EMPTY STAR
+
+
+
+*/
