fc93c0c8454ec8b2427bf0edbb774c398c4317ab
gdevgproject <gdev.gproject@gmail.com>
2024-12-05 14:15:09 +0700

    change key use popcorn

--- 10-use-popcorn/src/index.js
+++ 10-use-popcorn/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import StarRating from "./StartRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,10 +9,10 @@ root.render(
     <App />
     {/* <StarRating
       maxRating={5}
-      size={24}
-      color="red"
-      // messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-    /> */}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test /> */}
   </React.StrictMode>
 );
--- 10-use-popcorn/src/useMovies.js
+++ 10-use-popcorn/src/useMovies.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const KEY = "f84fc31d";
+const KEY = "3efe45ba";
 
 export function useMovies(query) {
   const [movies, setMovies] = useState([]);
